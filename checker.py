3cb2fa4ccc39d773b30f0b91e6cecd44ceac94c4
Cap. Hindsight <hindsight@yandex.ru>
2017-05-20 17:39:55 +1000

    deploy for testing

--- checker.py
+++ checker.py
@@ -28,8 +28,8 @@ class TokenizedChecker(Checker):
     return [token for token in pre_tokens if token != '']
 
   def check(self, input_file, output_file, answer_file):
-    output = extract_tokens(open(output_file, 'r').read())
-    answer = extract_tokens(open(answer_file, 'w').read())
+    output = TokenizedChecker.extract_tokens(open(output_file, 'r').read())
+    answer = TokenizedChecker.extract_tokens(open(answer_file, 'w').read())
     if output == answer:
       return TestRes(
         verdict=Verdict.OK,
